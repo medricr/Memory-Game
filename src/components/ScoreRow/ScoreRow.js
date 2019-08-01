bffe8c5ab43575d4e7f7a64a3c6b4d92209c6e13
Medric Riley <medricr@gmail.com>
2019-07-31 21:11:58 -0700

    fixed syntax error

--- src/components/ScoreRow/ScoreRow.js
+++ src/components/ScoreRow/ScoreRow.js
@@ -11,15 +11,7 @@ class ScoreRow extends React.Component {
 			<div>
 				<Navbar color="light">
 					<NavbarBrand>Memory Game!</NavbarBrand>
-					{/* <ul>
-						<li>
-							<NavbarBrand>Memory Game!</NavbarBrand>
-						</li>
-						<t> TEST YOUR MEMORY </t>
-						<li>
-							Score: {this.props.currentScore} | High Score: {this.props.highScore}
-						</li>
-					</ul> */}
+					
 					<t>
 						TEST YOUR MEMORY, ASHEN ONE
 					</t>
